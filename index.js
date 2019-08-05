644990e8c643fa1e346c97359174bca0fd4f10af
github username <jpsypek@gmail.com>
2019-08-05 15:52:47 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let min_i = 0
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] < min) {
+      min = array[i]
+      min_i = i
+    }
+  }
+  array.splice(min_i, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let sortedArray = []
+  while(array.length >= 1) {
+    let newMin = findMinAndRemove(array)
+    sortedArray.push(newMin)
+  }
+  return sortedArray
 }
